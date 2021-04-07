3b1632e1949c9d293e5fbb764b87abb7e4a7ec52
Sam Cowger <sam@galois.com>
2021-04-07 10:02:49 -0500

    Checkpoint

--- src/Language/ASKEE/ABM/Translate.hs
+++ src/Language/ASKEE/ABM/Translate.hs
@@ -75,25 +75,43 @@ stateDecl (ESLState ss) = ESL.State (mkName ss) (Var "???")
     mkName = Text.intercalate "_" . Map.elems
 
 
-foo expr = go expr Map.empty (error "foo") (\cur res -> cur) agent
+foo expr = search expr Map.empty [] (:) agent
 
+simpleEq1 :: ABM.AgentExpr
+simpleEq2 :: ABM.AgentExpr
+simpleEq3 :: ABM.AgentExpr
+simpleEq :: ABM.AgentExpr
 simpleEq1 = ABM.Eq (ABM.Attribute "x" "health") (ABM.Status "sus")
-simpleEq2 = ABM.Eq (ABM.Attribute "y" "health") (ABM.Status "exp")
-simpleEq3 = ABM.Eq (ABM.Attribute "y" "health") (ABM.Status "pdx")
+simpleEq2 = ABM.Eq (ABM.Attribute "y" "health") (ABM.Status "inf")
+simpleEq3 = ABM.Eq (ABM.Attribute "y" "health") (ABM.Status "rec")
 simpleEq = foldr1 ABM.Or [simpleEq1, simpleEq2, simpleEq3]
 
+complexEq1 :: ABM.AgentExpr
+complexEq2 :: ABM.AgentExpr
+complexEq3 :: ABM.AgentExpr
+complexEq4 :: ABM.AgentExpr
+complexEq5 :: ABM.AgentExpr
+complexEq6 :: ABM.AgentExpr
+complexEq :: ABM.AgentExpr
 complexEq1 = ABM.Eq (ABM.Attribute "x" "city") (ABM.Attribute "y" "city")
 complexEq2 = ABM.Eq (ABM.Attribute "x" "city") (ABM.Status "sea")
 complexEq3 = ABM.Eq (ABM.Attribute "y" "health") (ABM.Status "sea")
+complexEq4 = ABM.Eq (ABM.Attribute "x" "health") (ABM.Attribute "y" "health")
+complexEq5 = ABM.Eq (ABM.Attribute "x" "city") (ABM.Attribute "y" "city")
+complexEq6 = ABM.And complexEq4 complexEq5
 complexEq = foldr1 ABM.And [complexEq1, complexEq2, complexEq3]
 
-go :: ABM.AgentExpr 
-   -> Map Text ESLState
-   -> Map Text ESLState
-   -> (Map Text ESLState -> Map Text ESLState -> Map Text ESLState) 
-   -> ABM.Agent
-   -> Map Text ESLState
-go expr curr fail succ a@(ABM.Agent allAttrs) =
+e1 = ABM.And complexEq1 simpleEq1
+e2 = ABM.And simpleEq1 complexEq1
+
+
+search :: ABM.AgentExpr 
+       -> Map Text ESLState
+       -> [Map Text ESLState]
+       -> (Map Text ESLState -> [Map Text ESLState] -> [Map Text ESLState]) 
+       -> ABM.Agent
+       -> [Map Text ESLState]
+search expr curr fail succ a@(ABM.Agent allAttrs) =
   case expr of
     ABM.Eq (ABM.Attribute agentName agentAttr) (ABM.Status agentStatus) -> 
       doSimpleEq agentName agentAttr agentStatus
@@ -101,17 +119,21 @@ go expr curr fail succ a@(ABM.Agent allAttrs) =
       doSimpleEq agentName agentAttr agentStatus
     ABM.Eq (ABM.Attribute n1 a1) (ABM.Attribute n2 a2) | a1 == a2 ->
       doComplexEq n1 n2 a1
-    ABM.And e1 e2 -> go e1 curr fail (\cur res -> go e2 cur res succ a) a
-    ABM.Or e1 e2 -> go e1 curr (go e2 curr fail succ a) succ a
+    ABM.And e1 e2 -> search e1 curr fail (\cur res -> search e2 cur res succ a) a
+    ABM.Or e1 e2 -> search e1 curr (search e2 curr fail succ a) succ a
     _ -> undefined 
 
   where
-    doComplexEq :: Text -> Text -> Text -> Map Text ESLState
+    doComplexEq :: Text -> Text -> Text -> [Map Text ESLState]
     doComplexEq agentName1 agentName2 agentAttr =
       case (curr Map.!? agentName1, curr Map.!? agentName2) of
         (Nothing, Nothing) -> 
           -- These agents don't exist in the mapping
-          pickStatusForAgents potentialStatuses
+          randomStatuses potentialStatuses $
+            Map.fromList 
+              [ (agentName1, Map.empty)
+              , (agentName2, Map.empty)
+              ]
         (Just (ESLState currentAttrs), Nothing) ->
           -- agentName1 exists in the mapping - find a matching 
           -- status for agentName2
@@ -125,7 +147,12 @@ go expr curr fail succ a@(ABM.Agent allAttrs) =
           case (currentAttrs1 Map.!? agentAttr, currentAttrs2 Map.!? agentAttr) of
             (Nothing, Nothing) -> 
               -- ...and neither of them define the attribute in question
-              pickStatusForAgents potentialStatuses
+              -- randomStatus agentAttr potentialStatuses currentAttrs1 currentAttrs2
+              randomStatuses potentialStatuses $
+                Map.fromList 
+                  [ (agentName1, currentAttrs1)
+                  , (agentName2, currentAttrs2)
+                  ]
             (Just s, Nothing) ->
               -- ...and agentName1 defines the attribute in question
               let newAttrs = Map.singleton agentAttr s
@@ -144,21 +171,30 @@ go expr curr fail succ a@(ABM.Agent allAttrs) =
             _ -> fail
       where
         -- | From a list of potential statuses, pick the first and assign it
-        -- to the attribute in question for both agents
-        pickStatusForAgents :: [Text] -> Map Text ESLState
-        pickStatusForAgents _potentialStatuses =
-          case _potentialStatuses of
-            [] -> fail
+        -- to the attribute in question for agents' attributes in the 
+        -- `agents` map
+        randomStatuses :: [Text] -> Map Text (Map Text Text) -> [Map Text ESLState]
+        randomStatuses candidateStatuses agents =
+          case candidateStatuses of
+            [] -> 
+              -- No candidate statuses left to assign
+              fail
             (s:ss) -> 
-              let newAttrs = Map.singleton agentAttr s
+              -- Some candidate statuses left to assign
+              let newAgentMap = Map.foldrWithKey (fill s) curr agents
+              in  succ newAgentMap (randomStatuses ss agents)
+          where
+            fill :: Text -> Text -> Map Text Text -> Map Text ESLState -> Map Text ESLState
+            fill status agentName agentAttrs curr =
+              let newAttrs = Map.insert agentAttr status agentAttrs
                   newState = ESLState newAttrs
-                  newAgentMap = Map.insert agentName1 newState (Map.insert agentName2 newState curr)
-              in  succ newAgentMap (pickStatusForAgents ss)
+                  newAgentMap = Map.insert agentName newState curr
+              in  newAgentMap
 
         -- | Given a set of attribute-status bindings for one agent, either
         -- find the correct attribute-status binding for the other agent or,
         -- if the attribute doesn't exist in the bindings, pick a new one
-        makeStatusForAgent :: Map Text Text -> Text -> Map Text ESLState
+        makeStatusForAgent :: Map Text Text -> Text -> [Map Text ESLState]
         makeStatusForAgent boundAgentAttrs unboundAgentName =
           case boundAgentAttrs Map.!? agentAttr of
             Just s ->
@@ -171,7 +207,10 @@ go expr curr fail succ a@(ABM.Agent allAttrs) =
             Nothing -> 
               -- The already-bound agent has no status associated with
               -- this attribute
-              pickStatusForAgents potentialStatuses
+              randomStatuses potentialStatuses $
+                Map.fromList 
+                  [ (unboundAgentName, Map.empty)
+                  , (otherAgent unboundAgentName, boundAgentAttrs)]
 
         -- | All potential statuses for the attribute name provided 
         -- to `doComplexEq`
@@ -180,23 +219,36 @@ go expr curr fail succ a@(ABM.Agent allAttrs) =
           concatMap ABM.attributeStatuses $
             filter (\(ABM.AgentAttribute attr ss) -> attr == agentAttr) allAttrs
 
-    doSimpleEq :: Text -> Text -> Text -> Map Text ESLState
+        otherAgent :: Text -> Text
+        otherAgent a
+          | a == agentName1 = agentName2
+          | a == agentName2 = agentName1
+          | otherwise = error "attempted to find other agent for nonexistent agent"
+
+    doSimpleEq :: Text -> Text -> Text -> [Map Text ESLState]
     doSimpleEq agentName agentAttr agentStatus =
       case curr Map.!? agentName of
+        -- The agent doesn't exist in the mapping
         Nothing -> 
           let newAttrs = Map.singleton agentAttr agentStatus
               newState = ESLState newAttrs
               newAgentMap = Map.insert agentName newState curr
           in  succ newAgentMap fail
         Just (ESLState currentAttrs) ->
+          -- The agent does exist in the mapping...
           case currentAttrs Map.!? agentAttr of
-            Just s | s == agentStatus -> succ curr fail
+            Just s | s == agentStatus -> 
+              -- ...and defines the attribute correctly
+              succ curr fail
             Nothing ->
+              -- ...and doesn't define the attribute of interest
               let newAttrs = Map.insert agentAttr agentStatus currentAttrs
                   newState = ESLState newAttrs
                   newAgentMap = Map.insert agentName newState curr
               in  succ newAgentMap fail
-            _ -> fail
+            _ -> 
+              -- ...and defines the attribute incorrectly
+              fail
 
 
 factorial :: Int -> Int
