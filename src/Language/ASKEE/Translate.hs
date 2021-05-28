46df0709cf797a50581095f7022c32de51897df1
Sam Cowger <sam@galois.com>
2021-05-28 10:54:36 -0700

    Tentatively kneecap Translate

--- src/Language/ASKEE/Translate.hs
+++ src/Language/ASKEE/Translate.hs
@@ -2,7 +2,8 @@
 module Language.ASKEE.Translate where
 
 import           Language.ASKEE.Convert              ( converter )
-import           Language.ASKEE.Types
+import           Language.ASKEE.Types                ( ModelType(..)
+                                                     , Representation(..) )
 import qualified Language.ASKEE.Syntax               as ESL
 import qualified Language.ASKEE.DEQ.Syntax           as DEQ
 import qualified Language.ASKEE.RNet.Syntax          as RNet
@@ -56,62 +57,62 @@ instance SerializeModel Topology.Net where
 
 -------------------------------------------------------------------------------
 
-class AsESL a where
-  asESL :: a -> Either String ESL.Model
+-- class AsESL a where
+--   asESL :: a -> Either String ESL.Model
 
-instance AsESL ESL.Model where
-  asESL = $(converter (ESL Abstract) (ESL Abstract))
+-- instance AsESL ESL.Model where
+--   asESL = $(converter (ESL Abstract) (ESL Abstract))
 
--- XXX: This could be made to work partially
--- instance AsESL DEQ.DiffEqs where
---   asESL = $(converter (DEQ Abstract) (ESL Abstract))
+-- -- XXX: This could be made to work partially
+-- -- instance AsESL DEQ.DiffEqs where
+-- --   asESL = $(converter (DEQ Abstract) (ESL Abstract))
 
-instance AsESL RNet.ReactionNet where
-  asESL = $(converter (RNET Abstract) (ESL Abstract))
+-- instance AsESL RNet.ReactionNet where
+--   asESL = $(converter (RNET Abstract) (ESL Abstract))
 
--- XXX blocked by lack of DEQ -> ESL translation
--- instance AsESL Latex.Latex where
---   asESL = $(converter (LATEX Abstract) (ESL Abstract))
+-- -- XXX blocked by lack of DEQ -> ESL translation
+-- -- instance AsESL Latex.Latex where
+-- --   asESL = $(converter (LATEX Abstract) (ESL Abstract))
 
-instance AsESL Topology.Net where
-  asESL = $(converter (TOPO Abstract) (ESL Abstract))
+-- instance AsESL Topology.Net where
+--   asESL = $(converter (TOPO Abstract) (ESL Abstract))
 
--------------------------------------------------------------------------------
+-- -------------------------------------------------------------------------------
 
-class AsDEQ a where
-  asDEQ :: a -> Either String DEQ.DiffEqs
+-- class AsDEQ a where
+--   asDEQ :: a -> Either String DEQ.DiffEqs
 
-instance AsDEQ ESL.Model where
-  asDEQ = $(converter (ESL Abstract) (DEQ Abstract))
+-- instance AsDEQ ESL.Model where
+--   asDEQ = $(converter (ESL Abstract) (DEQ Abstract))
 
-instance AsDEQ DEQ.DiffEqs where
-  asDEQ = $(converter (DEQ Abstract) (DEQ Abstract))
+-- instance AsDEQ DEQ.DiffEqs where
+--   asDEQ = $(converter (DEQ Abstract) (DEQ Abstract))
 
-instance AsDEQ RNet.ReactionNet where
-  asDEQ = $(converter (RNET Abstract) (DEQ Abstract))
+-- instance AsDEQ RNet.ReactionNet where
+--   asDEQ = $(converter (RNET Abstract) (DEQ Abstract))
 
-instance AsDEQ Latex.Latex where
-  asDEQ = $(converter (LATEX Abstract) (DEQ Abstract))
+-- instance AsDEQ Latex.Latex where
+--   asDEQ = $(converter (LATEX Abstract) (DEQ Abstract))
 
-instance AsDEQ Topology.Net where
-  asDEQ = $(converter (TOPO Abstract) (DEQ Abstract))
+-- instance AsDEQ Topology.Net where
+--   asDEQ = $(converter (TOPO Abstract) (DEQ Abstract))
 
--------------------------------------------------------------------------------
+-- -------------------------------------------------------------------------------
 
-class AsRNet a where
-  asRNet :: a -> Either String RNet.ReactionNet
+-- class AsRNet a where
+--   asRNet :: a -> Either String RNet.ReactionNet
 
--- XXX: don't remember whether there are theoretical limitations here 
--- instance AsRNet ESL.Model where
---   asRNet = $(converter (ESL Abstract) (RNET Abstract))
+-- -- XXX: don't remember whether there are theoretical limitations here 
+-- -- instance AsRNet ESL.Model where
+-- --   asRNet = $(converter (ESL Abstract) (RNET Abstract))
 
--- XXX: blocked by lack of DEQ -> ESL translation
--- instance AsRNet DEQ.DiffEqs where
---   asRNet = $(converter (DEQ Abstract) (RNET Abstract))
+-- -- XXX: blocked by lack of DEQ -> ESL translation
+-- -- instance AsRNet DEQ.DiffEqs where
+-- --   asRNet = $(converter (DEQ Abstract) (RNET Abstract))
 
-instance AsRNet RNet.ReactionNet where
-  asRNet = $(converter (RNET Abstract) (RNET Abstract))
+-- instance AsRNet RNet.ReactionNet where
+--   asRNet = $(converter (RNET Abstract) (RNET Abstract))
 
--- XXX: blocked by lack of DEQ -> ESL translation
--- instance AsRNet Latex.Latex where
---   asRNet = $(converter (LATEX Abstract) (RNET Abstract))
+-- -- XXX: blocked by lack of DEQ -> ESL translation
+-- -- instance AsRNet Latex.Latex where
+-- --   asRNet = $(converter (LATEX Abstract) (RNET Abstract))
