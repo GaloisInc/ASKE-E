6e5e0169c84042ae8529b63de90cf54c9b2246cb
Ajay Eeralla <ajay.eeralla@gmail.com>
2021-06-03 11:10:11 -0700

    eremove parens in latex output

--- src/Language/ASKEE/Latex/Print.hs
+++ src/Language/ASKEE/Latex/Print.hs
@@ -21,8 +21,9 @@ printLatex DiffEqs {..} = vcat [lets, initials, rates]
     rates = vcat $ map ddtBinding (Map.toList deqRates)
 
     binding (ident, expr) = hsep [text (unpack ident), "=", printExpr expr]
-    ddtBinding (ident, expr) = hcat ["\\frac{d ", text (unpack ident), "}{dt} = ", printExpr expr]
     initBinding (i,e) = hsep [hcat [text (unpack i),parens (int 0)], "=", printExpr e]
+    ddtBinding (ident, expr) = hcat ["\\frac{d ", text (unpack ident), "}{dt} = ", printExpr expr]
+    
 
   
 -- | Specialized version of `ppExpr` in Language.ASKEE.Core
@@ -30,11 +31,11 @@ printExpr :: Expr -> Doc
 printExpr expr =
   case expr of
     NumLit d -> text $ printf "%f" d
-    Op1 Neg e' -> "-"PP.<>parens (pp e')
+    Op1 Neg e' -> "-"PP.<> pp e'
     e1 :+: e2 -> hsep [pp e1, "+", pp e2]
     e1 :-: e2 -> hsep [pp e1, "-", pp e2]
     e1 :*: e2 -> hsep [pp e1, "*", pp e2]
-    e1 :/: e2 -> hcat ["\\frac{", pp e1, "}{", pp e2, "}"]
+    e1 :/: e2 -> hcat ["\\frac{", printExpr e1, "}{", printExpr e2, "}"]
     Var v -> text $ unpack v
     _ -> 
       panic 
@@ -53,7 +54,7 @@ printExpr expr =
       case e of
         NumLit  _ -> 10
         BoolLit _ -> 10
-        Op1 Neg _ -> 0
+        Op1 Neg _ -> 7
         Op1 Not _ -> 0
         _ :+:   _ -> 6
         _ :-:   _ -> 6
