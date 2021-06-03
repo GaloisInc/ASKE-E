41e49e5f3f3e916bf61d8567b498259c3e4e7fcb
Sam Cowger <sam@galois.com>
2021-06-03 15:59:53 -0700

    That was redundant

--- src/Language/ASKEE/Core/Convert.hs
+++ src/Language/ASKEE/Core/Convert.hs
@@ -1,9 +1,7 @@
 module Language.ASKEE.Core.Convert where
 
-import           Data.Map   ( Map )
 import qualified Data.Map   as Map
 import           Data.Maybe ( mapMaybe )
-import qualified Data.Set   as Set
 
 import Language.ASKEE.Core.Syntax
 import Language.ASKEE.Core.Simplify ( simplifyExpr )
@@ -21,13 +19,6 @@ asDiffEqs mdl =
                $ foldr (:+:) (NumLit 0)
                $ mapMaybe (eventTerm sv)
                $ modelEvents mdl
-
-applyParams :: Map Ident Expr -> DiffEqs -> DiffEqs
-applyParams su = dropParams . mapExprs (substExpr su)
-  where
-  dropParams m = m { deqParams = [ x | x <- deqParams m
-                                 , not (x `Set.member` pSet) ] }
-  pSet = Map.keysSet su
 
 -- Eventually we may want to consider approaches to try to make guard
 -- continues (e.g., sigmoid?)
