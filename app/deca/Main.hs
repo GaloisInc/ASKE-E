7a65635af7006debd518fd25fb48dae2f4b17054
Sam Cowger <sam@galois.com>
2021-06-04 16:50:34 -0700

    Some work on deca

--- app/deca/Main.hs
+++ app/deca/Main.hs
@@ -1,4 +1,5 @@
 {-# Language BlockArguments, OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 module Main(main) where
 
 import qualified Data.Map as Map
@@ -12,91 +13,85 @@ import qualified Data.ByteString.Lazy.Char8 as LBS
 import Numeric(showGFloat)
 
 import qualified Language.ASKEE as A
-import qualified Language.ASKEE.Core as Core
-import           Language.ASKEE.DEQ.Syntax ( DiffEqs(..) )
-import           Language.ASKEE.DEQ.Print (ppDiffEqs)
-import qualified Language.ASKEE.Core.DiffEq as DiffEq
-import qualified Language.ASKEE.Core.GSLODE as ODE
-import qualified Language.ASKEE.DataSeries as DS
-import qualified Language.ASKEE.ESL.Print as PP
-import Language.ASKEE.RNet.Reaction (reactionsAsModel)
-import Language.ASKEE.Types
+-- import qualified Language.ASKEE.Core as Core
+-- import           Language.ASKEE.DEQ.Syntax ( DiffEqs(..) )
+-- import           Language.ASKEE.DEQ.Print (printDiffEqs)
+-- import qualified Language.ASKEE.Core.DiffEq as DiffEq
+-- import qualified Language.ASKEE.Core.GSLODE as ODE
+-- import qualified Language.ASKEE.DataSeries as DS
+-- import qualified Language.ASKEE.ESL.Print as PP
+-- import Language.ASKEE.RNet.Reaction (reactionsAsModel)
 
 import Options
+import Language.ASKEE.Error
+import qualified Language.ASKEE.DEQ as DEQ
 
 main :: IO ()
 main =
-  do opts <- getOptions
-     case command opts of
-      --  OnlyLex -> 
-      --     forM_ (modelFiles opts) (lexModel . FromFile)
-      --  OnlyParse -> 
-      --     forM_ (modelFiles opts) (parseModel . FromFile)
-      --  OnlyCheck ->
-      --     forM_ (modelFiles opts) (loadModel . FromFile)
-      --  DumpCPP -> 
-      --     forM_ (modelFiles opts) 
-      --       (genCppRunner . FromFile)
+  do  opts <- getOptions
+      case command opts of
+          
+      -- --  OnlyLex -> 
+      -- --     forM_ (modelFiles opts) (lexModel . FromFile)
+      -- --  OnlyParse -> 
+      -- --     forM_ (modelFiles opts) (parseModel . FromFile)
+      -- --  OnlyCheck ->
+      -- --     forM_ (modelFiles opts) (loadModel . FromFile)
+      -- --  DumpCPP -> 
+      -- --     forM_ (modelFiles opts) 
+      -- --       (genCppRunner . FromFile)
 
-       DumpDEQs ->
-         do ds <- exactlyOne "model" =<< loadDiffEqs opts []
-            print (ppDiffEqs ds)
+      --  DumpDEQs ->
+      --    do ds <- exactlyOne "model" =<< loadDiffEqs opts []
+      --       print (ppDiffEqs ds)
+        SimulateODE start stop step ->
+          do  (modelFile, modelType) <- exactlyOne "model-like thing" $ modelsProvided opts
+              res <- A.simulateModel modelType (A.FromFile modelFile) start stop step (overwrite opts)
+              let bs = A.dataSeriesAsCSV res
+                  out = outFile opts
+              if null out
+                then LBS.putStrLn bs
+                else LBS.writeFile out bs
+              when (gnuplot opts && not (null out)) $
+                writeFile (replaceExtension out "gnuplot") $ 
+                  A.gnuPlotScript res out
 
-       SimulateODE x y z ->
-         do m0 <- exactlyOne "model" =<< loadDiffEqs opts []
-            let m     = DiffEq.applyParams (Core.NumLit <$> overwrite opts) m0
-                res   = simODE m x y z
-                bs    = DS.encodeDataSeries res
-                ofile = outFile opts
-            if null ofile
-              then LBS.putStrLn bs
-              else LBS.writeFile (outFile opts) bs
-            when (gnuplot opts && not (null ofile)) $
-              writeFile (replaceExtension ofile "gnuplot")
-                $ gnuPlotScript res ofile
+        ComputeError ->
+          do  eqss <- mapM (A.loadDiffEqs . A.FromFile) (deqFiles opts)
+              forM_ eqss \eqs ->
+                forM_ (dataFiles opts) \d ->
+                  do  putStrLn ("  data: " ++ show d)
+                      ds <- A.parseDataSeriesFromFile d
+                      let errs = DEQ.computeErrorPerVar
+                               $ DEQ.modelSquareError eqs ds Map.empty
+                      forM_ (Map.toList errs) \(x,e) ->
+                        putStrLn ("    " ++ Text.unpack x ++ ": " ++ show e)
 
-       ComputeError ->
-         do eqss <- loadDiffEqs opts []
-            forM_ eqss \eqs ->
-              forM_ (dataFiles opts) \d ->
-                do putStrLn ("  data: " ++ show d)
-                   ds <- DS.parseDataSeriesFromFile d
-                   let errs = ODE.computeErrorPerVar
-                            $ ODE.modelSquareError eqs ds Map.empty
-                   forM_ (Map.toList errs) \(x,e) ->
-                     putStrLn ("    " ++ Text.unpack x ++ ": " ++ show e)
+        FitModel ps scale ->
+          do  (modelFile, modelType) <- exactlyOne "model-like thing" $ modelsProvided opts
+              equations <- A.loadDiffEqsFrom modelType (A.FromFile modelFile)
+              dataFile <- exactlyOne "data file" (dataFiles opts)
+              ds <- A.parseDataSeriesFromFile dataFile
+              (res,work) <- A.fitModelToData modelType (A.FromFile dataFile) ps scale (A.FromFile modelFile)
+              let showF f = showGFloat Nothing f ""
+                  see n xs =
+                    do  putStrLn n
+                        forM_ (Map.toList xs) \(x,(y,e)) ->
+                          putStrLn ("let " ++ Text.unpack x ++
+                                    " = " ++ showF y ++
+                                    " # error = " ++ showF e)
+              forM_ (zip [ (1::Int) .. ] work) \(n,ys) ->
+                do  putStrLn ("-- Step " ++ show n ++ " --")
+                    forM_ (Map.toList ys) \(x,y) ->
+                      putStrLn ("let " ++ Text.unpack x ++
+                                " = " ++ showF y)
+              see "Result:" res
+              let totalErr = DEQ.computeErrorPerVar
+                           $ DEQ.modelSquareError equations ds (fst <$> res)
+              forM_ (Map.toList totalErr) \(x,e) ->
+                putStrLn $ "# error in " ++ Text.unpack x ++ " = " ++ showF e
 
-       FitModel ps scale ->
-         case dataFiles opts of
-           [df] ->
-              do eqs <- exactlyOne "model" =<< loadDiffEqs opts ps
-                 ds  <- DS.parseDataSeriesFromFile df
-                 let showF f = showGFloat Nothing f ""
-                 let (res,work) = ODE.fitModel eqs ds scale
-                                          (Map.fromList (zip ps (repeat 0)))
-                     see n xs =
-                       do putStrLn n
-                          forM_ (Map.toList xs) \(x,(y,e)) ->
-                              putStrLn ("let " ++ Text.unpack x ++
-                                        " = " ++ showF y ++
-                                        " # error = " ++ showF e
-                                       )
-                 forM_ (zip [ (1::Int) .. ] work) \(n,ys) ->
-                       do putStrLn ("-- Step " ++ show n ++ " --")
-                          forM_ (Map.toList ys) \(x,y) ->
-                              putStrLn ("let " ++ Text.unpack x ++
-                                        " = " ++ showF y)
-
-                 see "Result:" res
-                 let totalErr = ODE.computeErrorPerVar
-                              $ ODE.modelSquareError eqs ds (fst <$> res)
-                 forM_ (Map.toList totalErr) \(x,e) ->
-                    putStrLn $ "# error in " ++ Text.unpack x ++
-                                                        " = " ++ showF e
-
-           _ -> throwIO (GetOptException
-                           ["Fitting needs 1 model and 1 data file. (for now)"])
-
+        _ -> throwIO (GetOptException [""])
 
   `catches`
   [ Handler  \(GetOptException errs) ->
@@ -113,29 +108,6 @@ exactlyOne thing xs =
     [a] -> pure a
     _   -> throwIO (GetOptException [ "Expected exactly 1 " ++ thing ])
 
-
-loadDiffEqs :: Options -> [Text] -> IO [DiffEqs]
-loadDiffEqs opts params =
-  do  ms1 <- mapM (fromEquations . FromFile) (deqFiles opts)
-      ms2 <- mapM (fromModel . FromFile)     (modelFiles opts)
-      ms3 <- mapM (fromRNet . FromFile)      (rnetFiles opts)
-      pure (ms1 ++ ms2 ++ ms3)
-  where
-    fromModel     = A.loadDiffEqsFrom (ESL Concrete) (overwrite opts) params
-    fromRNet      = A.loadDiffEqsFrom (RNET Concrete) (overwrite opts) params
-    fromEquations = A.loadDiffEqsFrom (DEQ Concrete) (overwrite opts) params
-
-simODE :: DiffEqs -> Double -> Double -> Double -> DS.DataSeries Double
-simODE m start step end = ODE.simulate m Map.empty times
-  where times = takeWhile (<= end) (iterate (+ step) start)
-
-gnuPlotScript :: DS.DataSeries Double -> FilePath -> String
-gnuPlotScript ds f =
-  unlines
-    [ "set key outside"
-    , "set datafile separator \",\""
-    , "plot for [col=2:" ++ show (DS.dsColumns ds) ++ "] " ++
-         show f ++ " using 1:col with lines title columnheader"
-    ]
-
-
+modelsProvided :: Options -> [(FilePath, A.ModelType)]
+modelsProvided opts =
+  map (, A.DeqType) (deqFiles opts) ++ map (, A.EaselType) (modelFiles opts)
