02c42f11789b89c243121b445c29978274efaff6
David B. Lamkins <dlamkins@galois.com>
2021-06-11 13:24:12 -0700

    Correct HERE. Check CLI args. Usage. Allow space in paths on CLI. Support gather of files in directory.

--- loe-driver.sh
+++ loe-driver.sh
@@ -2,25 +2,39 @@
 
 ### Test harness to exercise LOE calculations.
 
-HERE=$(cd `basename $0`; pwd)
+HERE=$(cd `dirname $0`; pwd)
 OPTS=
 
+[ $# -ge 2 ] || {
+	echo "usage: $0 [-t|--tokens] FILE_OR_DIR FILE_OR_DIR..."
+	exit 1
+}
+
+[ "$1" = -t -o "$1" = --tokens ] && { OPTS=$1; shift; }
 function remark { echo "$@"; }
-function gather { gf=`mktemp`; find $1 -type f -exec cat \{\} \; > $gf; echo $gf; }
-function cmp { $HERE/loe-metrics.py $OPTS $1 $2; }
+function gather { gf=`mktemp`; find "$*" -type f -exec cat \{\} \; > $gf; echo $gf; }
+function cmp { $HERE/loe-metrics.py $OPTS "$1" "$2"; }
 function cmpderiv {
 	remark Derivations:
-	for f in $*; do
+	for f in "$@"; do
 		remark "  $f"
 	done
 	while test $# -ge 2; do
-		f1=$1
+		f1="$1"
 		remark == Basis: $f1
 		step=0
-		for f2 in $*; do
+		for f2 in "$@"; do
 			remark -- $step step
 			step=$(($step+1))
-			cmp $f1 $f2
+			test -d "$f1" && {
+				f1="$(gather $f1)"
+				remark gather: $1 is $f1
+			}
+			test -d "$f2" && {
+				f2="$(gather $f2)"
+				remark gather: $2 is $f2
+			}
+			cmp "$f1" "$f2"
 		done
 		shift
 	done
