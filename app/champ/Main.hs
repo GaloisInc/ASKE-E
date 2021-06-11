a3804e18962a00feb9b4f09306674266952d3ffd
Ryan Scott <rscott@galois.com>
2021-06-11 10:16:59 -0400

    Haskeline-based REPL

---
+++ app/champ/Main.hs
@@ -0,0 +1,127 @@
+module Main (main) where
+
+import Control.Applicative ((<**>))
+import Control.Monad (when)
+import Control.Monad.IO.Class (MonadIO(..))
+import Data.Foldable
+import qualified Data.List.Extra as L
+import qualified Options.Applicative as Opt
+import System.Console.Haskeline
+
+import Language.ASKEE.Jupyter.GenLexer (lexJupyter)
+import Language.ASKEE.Jupyter.GenParser (parseJupyterStmt)
+import qualified Language.ASKEE.Jupyter.Interpreter as Jupyter
+
+import Logo (displayLogo)
+
+main :: IO ()
+main = do
+  opts <- Opt.execParser champInfo
+  champ opts
+  where
+    champInfo :: Opt.ParserInfo Options
+    champInfo = Opt.info (optionsParser <**> Opt.helper)
+      ( Opt.fullDesc
+     <> Opt.progDesc spiel
+     <> Opt.header spiel )
+
+    spiel = "A simple REPL"
+
+champ :: Options -> IO ()
+champ opts = do
+  displayLogo (optColor opts) (optUnicode opts)
+  runInputT defaultSettings $ withInterrupt $ loop
+
+data Options = Options
+  { optColor   :: Bool
+  , optUnicode :: Bool
+  } deriving Show
+
+optionsParser :: Opt.Parser Options
+optionsParser = Options
+  <$> (fmap not . Opt.switch)
+      (  Opt.long "no-color"
+      <> Opt.help "Print the champ logo without color" )
+  <*> (fmap not . Opt.switch)
+      (  Opt.long "no-unicode"
+      <> Opt.help "Print the champ logo without Unicode" )
+
+loop :: InputT IO ()
+loop = do
+  mbInput <- handleInterrupt (handleCtrlC Nothing) $ getInputLine "champ> "
+  case mbInput of
+    Nothing -> loop
+    Just input -> do
+      keepGoing <- liftIO $ runCommand $ L.trim input
+      when keepGoing loop
+
+runCommand :: String -> IO Bool
+runCommand command =
+  case command of
+    ":" -> pure True
+    builtinCmd@(':':builtinCmdName) -> do
+      let possibleBuiltins = filter (\c -> any (builtinCmdName `L.isPrefixOf`)
+                                               (commandNames c))
+                                    builtins
+      case possibleBuiltins of
+        []        -> do putStrLn $ "Unknown command: " ++ builtinCmd
+                        pure True
+        [builtin] -> commandAction builtin
+        (_:_)     -> do putStrLn $ unlines
+                          [ builtinCmd ++ " is ambiguous, it could mean one of:"
+                          , "\t" ++ L.intercalate ", "
+                                    (map (':':) (concatMap commandNames possibleBuiltins))
+                          ]
+                        pure True
+    _ -> do
+      jupyterStmt command
+      pure True
+
+data Command = Command
+  { commandNames  :: [String]
+  , commandHelp   :: String
+  , commandAction :: IO Bool
+  }
+
+builtins :: [Command]
+builtins =
+  [ Command ["?", "help"]
+            "Display brief descriptions of each command."
+            (keepGoing helpCmd)
+  , Command ["q", "quit"]
+            "Exit the REPL."
+            (stop quitCmd)
+  ]
+  where
+    keepGoing :: IO () -> IO Bool
+    keepGoing action = action *> pure True
+
+    stop :: IO () -> IO Bool
+    stop action = action *> pure False
+
+helpCmd :: IO ()
+helpCmd = traverse_ showHelp builtins
+  where
+    showHelp :: Command -> IO ()
+    showHelp command = do
+      putStr "  "
+      putStr $ L.intercalate ", "
+             $ map (':':)
+             $ commandNames command
+      putStr "\t"
+      putStrLn $ commandHelp command
+
+quitCmd :: IO ()
+quitCmd = pure ()
+
+-- TODO: Make this smarter
+jupyterStmt :: String -> IO ()
+jupyterStmt code =
+  case lexJupyter code >>= parseJupyterStmt of
+    Left err   -> putStrLn err
+    Right stmt -> print $ Jupyter.interpretStmt stmt
+
+handleCtrlC :: a -> InputT IO a
+handleCtrlC x = do
+  liftIO $ putStrLn "Ctrl-C"
+  pure x
