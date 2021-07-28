759a783d26eca6c04eea7f5de3e0266ad50a7da4
David B. Lamkins <dlamkins@galois.com>
2021-07-28 16:50:41 -0700

    Remove lizard snapshot; add lizard setup script.

--- loe/mccabe/lizard-ccm.sh
+++ loe/mccabe/lizard-ccm.sh
@@ -5,8 +5,7 @@
 	echo "usage: $0 FILE"
 	exit 1
 }
-HERE=$(cd `dirname $0`; pwd)
 echo "$1"
-python $HERE/lizard-1.17.9/lizard.py -Eoutside -ENS "$1" \
+lizard -Eoutside -ENS "$1" \
 	|sed 's/^=\+$//'|awk '/^.* file analyzed/ {exit} {print}' \
-	|sed 's|^\(.*\)@.*$|\1|'
+	|sed 's|^\(.*\)@.*$|\1|'|cut -c11-16,40-
