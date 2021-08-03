80a27aa9b25ab11786fee36aef46ce1ade139488
Sam Cowger <sam@galois.com>
2021-08-03 08:15:08 -0700

    Spruce up join's argument interpretation

--- src/Language/ASKEE/Exposure/Interpreter.hs
+++ src/Language/ASKEE/Exposure/Interpreter.hs
@@ -330,21 +330,15 @@ interpretCall fun args =
     FJoin ->
       case args of
         [VModelExpr (EVal (VModel m1)), VModelExpr (EVal (VModel m2)), VArray vs]
-          | all good vs -> 
+          | Just vs' <- twoStrsM `traverse` vs -> 
             pure $
             VModel $
-            modelAsCore (join (mapify vs) (coreAsModel m1) (coreAsModel m2) )
+            modelAsCore (join (Map.fromList vs') (coreAsModel m1) (coreAsModel m2) )
           | otherwise -> typeError "all variable joins must be two-element arrays of strings"
         _ -> typeError $ "join expects two models and a list of variables to join" <> Text.pack (show args)
       where
-        good (VArray [VString _, VString _]) = True
-        good _ = False
-
-        mapify vs = 
-          Map.fromList 
-            [ (v1, v2) 
-            | VArray [VString v1, VString v2] <- vs
-            ]
+        twoStrsM (VArray [VString s1, VString s2]) = Just (s1, s2)
+        twoStrsM _ = Nothing
 
     FMean ->
       case args of
