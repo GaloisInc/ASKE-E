83c3fb4e1957f7a34311d17f655260f979a48a2e
Alexander Bakst <abakst@galois.com>
2021-08-03 09:50:51 -0700

    Exposure `fit` testcase

--- test/Exposure.hs
+++ test/Exposure.hs
@@ -4,6 +4,7 @@ module Exposure (tests) where
 import qualified Data.ByteString.Lazy as LBS
 import Data.Bifunctor (Bifunctor(..))
 import qualified Data.Text as Text
+import qualified Data.Map as Map
 import Data.Text (Text)
 import System.FilePath ((</>))
 import Test.Tasty.HUnit
@@ -13,7 +14,20 @@ import Language.ASKEE.Exposure.GenLexer (lexExposure)
 import Language.ASKEE.Exposure.GenParser (parseExposureExpr, parseExposureStmt)
 import Language.ASKEE.Exposure.Interpreter
 import Language.ASKEE.Exposure.Syntax
+
 import Paths_aske_e (getDataDir)
+import Language.ASKEE (DataSeries(..))
+
+series1 :: DataSeries Double
+series1 =
+  DataSeries { times = [0,30,60,90,120]
+             , values = Map.fromList
+                [ ("I",[3,570.9758710681082,177.87795797377797,53.663601453388395,16.17524903479719])
+                , ("S",[997,16.03663576555767,0.2688016239687885,7.747202089688689e-2,5.323898868597058e-2])
+                , ("R",[0,412.9874931663346,821.8532404022534,946.258926525715,983.771511976517])
+                , ("total_population",[1000,1000,1000,1000])
+                ]
+             }
 
 assertLeft :: Either a b -> IO ()
 assertLeft (Left _)  = pure ()
@@ -197,5 +211,15 @@ tests =
             case v of
               VDouble 5.0 -> pure ()
               _ -> assertFailure "x not 5.0"
+      , testCase "Param fitting (success)" $ do
+          loadSirEaselExpr <- getLoadSirEaselExpr
+          exprAssertionWithStmts
+            [ "sir = " <> loadSirEaselExpr
+            , "series = simulate(sir at [0..120 by 30])"
+            , "ps = fit(sir, series, \"i_initial\")"
+            ] "ps.values.I_initial" $ \val ->
+            case val of
+              VPoint _ -> pure ()
+              _ -> assertFailure "fit did not return a point"
       ]
     ]
