3bba10c906b9b3f6217627afb1163cec1e29b7c9
Ryan Scott <rscott@galois.com>
2021-08-09 14:56:04 -0400

    Exposure: Allow specifying color schemes/ranges
    Vega-Lite has a dizzying array of color options. For now, we allow specifying
    colors in one of two ways:
    
    * A color scheme, which is a predefined, named set of colors to alternate
      between. These are represented as strings.
    * A color range, which is a user-defined set of colors to alternate between.
      These are represented as arrays of strings.
    
    Fixes #91.

--- src/Language/ASKEE/Exposure/Plot.hs
+++ src/Language/ASKEE/Exposure/Plot.hs
@@ -20,7 +20,7 @@ data PlotSeries a = PlotSeries
   { plotSeriesLabel :: Text
   , plotSeriesData  :: a
   , plotSeriesStyle :: PlotStyle
-  , plotColor       :: Maybe Text
+  , plotColor       :: Maybe PlotColor
   }
   deriving (Show, Eq, Ord, Generic, NFData)
 
@@ -31,8 +31,21 @@ data PlotStyle
   | Line
   deriving (Show, Eq, Ord, Generic, NFData)
 
+data PlotColor
+  = ColorScheme Text
+    -- ^ A color scheme, as described in
+    --   <https://vega.github.io/vega/docs/schemes/>.
+  | ColorRange [Text]
+    -- ^ A range of colors, as described in
+    --   <https://vega.github.io/vega-lite/docs/scale.html#2-setting-the-range-property-to-an-array-of-valid-css-color-strings>.
+  deriving (Show, Eq, Ord, Generic, NFData)
+
 instance JS.ToJSON PlotStyle where
   toJSON Points  = "point"
   toJSON Circles = "circle"
   toJSON Squares = "square"
   toJSON Line    = "line"
+
+instance JS.ToJSON PlotColor where
+  toJSON (ColorScheme scheme) = JS.toJSON scheme
+  toJSON (ColorRange range)   = JS.toJSON range
