3bf8a9a7db58d4b328fe549695b84f5c9d0b9d5e
Sam Cowger <sam@galois.com>
2021-09-13 17:01:42 -0700

    Fix bug re. combination involving events acting on one agent

--- src/Language/ASKEE/ABM/Manipulate.hs
+++ src/Language/ASKEE/ABM/Manipulate.hs
@@ -2,11 +2,12 @@
 {-# LANGUAGE RecordWildCards #-}
 module Language.ASKEE.ABM.Manipulate where
 
-import           Data.List  ( nub )
-import qualified Data.Map   as Map
-import qualified Data.Text  as Text
+import           Data.List ( nub )
+import qualified Data.Map  as Map
+import qualified Data.Text as Text
 
 import Language.ASKEE.ABM.Syntax
+import Language.ASKEE.Panic      ( panic )
 
 combine :: Model -> Model -> Model
 combine m1 m2 = Model name agent lets initial events
@@ -31,18 +32,28 @@ combine m1 m2 = Model name agent lets initial events
       map mkNewM1Event (modelEvents m1) ++
       map mkNewM2Event (modelEvents m2)
 
-    m1NonminglingStatuses = [attrName | (attrName, AgentAttribute n Nonmingling s) <- Map.toList (modelAgent m1)]
-    m2NonminglingStatuses = [attrName | (attrName, AgentAttribute n Nonmingling s) <- Map.toList (modelAgent m2)]
+    m1NonminglingAttrs = [attrName | (attrName, AgentAttribute _ Nonmingling _) <- Map.toList (modelAgent m1)]
+    m2NonminglingAttrs = [attrName | (attrName, AgentAttribute _ Nonmingling _) <- Map.toList (modelAgent m2)]
 
     mkNewM1Event e@Event{..} =
-      case m2NonminglingStatuses of
-        [] -> e
-        ss -> error (show ss) e { eventWhen = foldr1 And (map (\attr -> foldr1 And (zipWith (\a1 a2 -> Eq (Attribute a1 attr) (Attribute a2 attr)) eventAgents (tail eventAgents))) ss) }
-    mkNewM2Event e@Event{..} =
-      case m1NonminglingStatuses of
-        [] -> e
-        ss -> e { eventWhen = foldr1 And (map (\status -> foldr1 And (zipWith (\a1 a2 -> Eq (Attribute a1 status) (Attribute a2 status)) eventAgents (tail eventAgents))) ss) }
+      case (m2NonminglingAttrs, eventAgents) of
+        ([], _) -> e
+        (_, [_]) -> e
+        (_, []) -> panic "mkNewM1Event" ["event acted on no agents"]
+        (as, _) -> 
+          let equateAgents attr = foldr1 And (zipWith (equateAttrs attr) eventAgents (tail eventAgents))
+          in  e { eventWhen = eventWhen `And` foldr1 And (map equateAgents as) }
 
+    mkNewM2Event e@Event{..} =
+      case (m1NonminglingAttrs, eventAgents) of
+        ([], _) -> e
+        (_, [_]) -> e
+        (_, []) -> panic "mkNewM2Event" ["event acted on no agents"]
+        (as, _) -> 
+          let equateAgentAttrs attr = foldr1 And (zipWith (equateAttrs attr) eventAgents (tail eventAgents))
+          in  e { eventWhen = eventWhen `And` foldr1 And (map equateAgentAttrs as) }
+
+    equateAttrs attr agent1 agent2 = Eq (Attribute agent1 attr) (Attribute agent2 attr)
 
 -- | Two agents are compatible if they don't share names of attributes and if
 -- those attributes' statuses don't have name clashes
